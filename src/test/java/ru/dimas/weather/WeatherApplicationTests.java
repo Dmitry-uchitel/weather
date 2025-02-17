9a6b04fbd61dcf6015ee82adbadcbd957785253f
Dimas <shelop_1@mail.ru>
2025-02-17 11:28:38 +0800

    afterFrontend+before restrukturizaciya

---
+++ src/test/java/ru/dimas/weather/WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dimas.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
